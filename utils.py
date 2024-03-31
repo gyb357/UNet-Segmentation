87406df70d2a136fcce7c723d53b856419faf256
gyb357 <gyb357@naver.com>
2024-03-31 19:38:24 +0900

    no message

--- utils.py
+++
@@ -1,9 +0,0 @@
-class TernaryOperator():
-    @staticmethod
-    def operate(a: bool, b, c):
-        return b if a is True else c
-    
-    @staticmethod
-    def operate_elif(a: bool, b, c: bool, d, e):
-        return b if a is True else (d if c is True else e)
-
