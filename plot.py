0887372a10d2438983e39d227d682abfeb25395d
gyb357 <gyb357@naver.com>
2024-09-07 19:55:53 +0900

    no message

--- plot.py
+++ plot.py
@@ -5,18 +5,23 @@ import matplotlib.pyplot as plt
 file_path = 'csv/train_logg.csv'
 df = pd.read_csv(file_path)
 
-plt.figure()
+
+plt.figure(1)
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
 plt.plot(df['Epoch'], df['Train_loss'], label='Train Loss')
 plt.plot(df['Epoch'], df['Val_loss'], label='Validation Loss')
-plt.plot(df['Epoch'], df['Train_metrics'], label='Train metrics')
-plt.plot(df['Epoch'], df['Val_metrics'], label='Validation metrics')
-
-plt.title('Training and Validation Loss/Metrics over Epochs')
-
-plt.xlabel('Epoch')
-plt.ylabel('Value')
-
 plt.legend()
+
+
+plt.figure(2)
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.plot(df['Epoch'], df['Train_metrics'], label='Train Accuracy')
+plt.plot(df['Epoch'], df['Val_metrics'], label='Validation Accuracy')
+plt.legend()
+
+
 plt.tight_layout()
 plt.show()
 
